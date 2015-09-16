c4d51ca1e9b2322837a75584c3867d7b52de7332
Simon Legg <leggsimon@gmail.com>
2015-09-16 09:51:14 +0100

    set up express server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var root = __dirname;
+
+app.use(express.static(process.cwd() + '/app'));
+app.use(express.static(root + '/views'));
+app.use('/bower_components', express.static(path.join(root, 'bower_components')));
+app.use('/app', express.static(path.join(root, 'app')));
+app.use(express.static(process.cwd() + '/app'));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(root + '/app/views/index.html'));
+});
+
+server.listen(3000, function(){
+  console.log("Server listening on port 3000");
+});
+
+module.exports = server;
